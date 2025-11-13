c07eb7e374d5dc75db78ee759930e5c09b00d021
Luissmont <luispm2424@gmail.com>
2025-11-12 18:04:35 -0600

    feat(splash): Initial SplashScreen composable with logo and text.

--- app/src/main/java/com/mobiles/bioequip_des/Presentation/Views/Auth/SplashScreen.kt
+++ app/src/main/java/com/mobiles/bioequip_des/Presentation/Views/Auth/SplashScreen.kt
@@ -1,2 +1,53 @@
 package com.mobiles.bioequip_des.Presentation.Views.Auth
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mobiles.bioequip_des.R
+import com.mobiles.bioequip_des.Presentation.ui.theme.BioequipTeal
+
+@Composable
+fun SplashScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_bioequiphub),
+                contentDescription = "Logo de Bioequip Hub",
+                modifier = Modifier.size(150.dp)
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Text(
+                text = "BIOEQUIP HUB",
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen()
+}
