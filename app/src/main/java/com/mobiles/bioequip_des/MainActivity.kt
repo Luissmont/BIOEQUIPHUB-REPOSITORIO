bedf1fa62b1935da1b5bb56a11d9cce60fda1592
Luissmont <luispm2424@gmail.com>
2025-11-12 20:33:52 -0600

    feat(register): Initial clean RegisterScreen structure, set as entry point, respecting Views/Auth architecture.

--- app/src/main/java/com/mobiles/bioequip_des/MainActivity.kt
+++ app/src/main/java/com/mobiles/bioequip_des/MainActivity.kt
@@ -3,34 +3,26 @@ package com.mobiles.bioequip_des
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.mobiles.bioequip_des.Presentation.Views.Auth.SplashScreen
+import com.mobiles.bioequip_des.Presentation.Views.Auth.RegisterScreen
 import com.mobiles.bioequip_des.Presentation.ui.theme.BIOEQUIPDESTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             BIOEQUIPDESTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ){
-                    SplashScreen()
-                }
-
-
-
+                RegisterScreen()
             }
         }
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BIOEQUIPDESTheme {
+        RegisterScreen()
+    }
+}
--- app/src/main/java/com/mobiles/bioequip_des/Presentation/Views/Auth/RegisterScreen.kt
+++ app/src/main/java/com/mobiles/bioequip_des/Presentation/Views/Auth/RegisterScreen.kt
@@ -1,2 +1,28 @@
 package com.mobiles.bioequip_des.Presentation.Views.Auth
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mobiles.bioequip_des.Presentation.ui.theme.BIOEQUIPDESTheme
+
+@Composable
+fun RegisterScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("PANTALLA DE REGISTRO EN CONSTRUCCIÓN")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RegisterScreenPreview() {
+    BIOEQUIPDESTheme {
+        RegisterScreen()
+    }
+}
