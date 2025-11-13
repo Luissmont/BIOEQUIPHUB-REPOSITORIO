bd155587a596742ad87c38f6d2cb07873f1d898a
Luissmont <luispm2424@gmail.com>
2025-11-12 18:32:18 -0600

    refactor(main): Show SplashScreen as initial view.

--- app/src/main/java/com/mobiles/bioequip_des/MainActivity.kt
+++ app/src/main/java/com/mobiles/bioequip_des/MainActivity.kt
@@ -5,12 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.mobiles.bioequip_des.Presentation.Views.Auth.SplashScreen
 import com.mobiles.bioequip_des.Presentation.ui.theme.BIOEQUIPDESTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BIOEQUIPDESTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    SplashScreen()
                 }
+
+
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BIOEQUIPDESTheme {
-        Greeting("Android")
-    }
-}
