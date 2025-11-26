7c17fe549284d1f9c1495aa54ccfad2537c8694c
Luissmont <luispm2424@gmail.com>
2025-11-25 21:34:28 -0600

    equipment model

--- app/src/main/java/com/mobiles/bioequip_des/Data/Models/Equipment.kt
+++ app/src/main/java/com/mobiles/bioequip_des/Data/Models/Equipment.kt
@@ -1,4 +1,23 @@
 package com.mobiles.bioequip_des.Data.Models
 
-class Equipment {
-}
+data class Equipment(
+    val id: String = "",
+    val registryId: String = "",
+    val name: String = "",
+    val serialNumber: String = "",
+    val registrationNumber: String = "",
+    val inventoryNumber: String = "",
+    val manufacturerNumber: String = "",
+    val brand: String = "",
+    val model: String = "",
+    val budgetItem: String = "",
+    val series: String = "",
+    val physicalLocation: String = "",
+    val serviceArea: String = "",
+    val classification: String = "",
+    val medicalUnit: String = "",
+    val photoUrl: String = "",
+    val status: String = "disponible",
+    val createdAt: Long = System.currentTimeMillis(),
+    val createdBy: String = ""
+)
